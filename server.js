80ff77502a74b66fba287563da4243db8b7e62bb
jasontttttt <89667970+jasontttttt@users.noreply.github.com>
2023-04-10 01:13:45 +0800

    adding chatRoute

---
+++ server.js
@@ -0,0 +1,34 @@
+// Express
+const express = require('express');
+const app = express();
+
+// CORS
+var cors = require('cors');
+app.use(cors());
+
+// .env
+require('dotenv').config();
+
+// Body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set up the server to listen on port 3000
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+// Set up the router
+const userRouter = require('./user/route');
+app.use('/user', userRouter);
+
+const tweetRouter = require('./tweet/route');
+app.use('/tweet', tweetRouter);
+
+const acRouter = require('./accessControl/route');
+app.use('/', acRouter);
+
+const chatRouter = require("./chat/chat");
+app.use("/chat", chatRouter);
