ee87609ecaf89c8e518467f2b92f855d25570af3
jasontttttt <jasontytai@yahoo.com.hk>
2023-04-10 01:12:10 +0800

    Changes to be committed: 	modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,18 @@
-const express = require("express");
-const path = require("path");
+// Express
+const express = require('express');
 const app = express();
-const {
-  connection,
-  connectionPromise,
-  query,
-  executeQuery,
-} = require("./database");
-require("dotenv").config();
-const cors = require("cors");
+
+// CORS
+var cors = require('cors');
 app.use(cors());
+
+// .env
+require('dotenv').config();
+
+// Body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Set up the server to listen on port 3000
 const port = process.env.PORT || 3000;
@@ -18,18 +21,14 @@ app.listen(port, () => {
 });
 
 // Set up the router
-const userRouter = require("./user/route");
-app.use("/user", userRouter);
+const userRouter = require('./user/route');
+app.use('/user', userRouter);
 
-const tweetRouter = require("./tweet/route");
-app.use("/tweet", tweetRouter);
+const tweetRouter = require('./tweet/route');
+app.use('/tweet', tweetRouter);
 
-const acRouter = require("./accessControl/route");
-app.use("/", acRouter);
+const acRouter = require('./accessControl/route');
+app.use('/', acRouter);
 
 const chatRouter = require("./chat/chat");
 app.use("/chat", chatRouter);
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
