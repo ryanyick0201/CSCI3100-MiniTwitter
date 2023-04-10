16c38bb6afbdbb1acab2b2956f09101625a022ba
jasontttttt <jasontytai@yahoo.com.hk>
2023-04-11 02:22:21 +0800

    changed image.js to async

--- server/multimedia/image.js
+++ server/multimedia/image.js
@@ -1,12 +1,17 @@
-const { S3Client, PutObjectCommand, DeleteObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const {
+  S3Client,
+  PutObjectCommand,
+  DeleteObjectCommand,
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const bucketName = process.env.AWS_BUCKET_NAME
-const region = process.env.AWS_BUCKET_REGION
-const accessKeyId = process.env.AWS_ACCESS_KEY
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
 // console.log(`\n${bucketName}\n`);
 // console.log(`\n${region}\n`);
@@ -17,54 +22,57 @@ const s3Client = new S3Client({
   region,
   credentials: {
     accessKeyId,
-    secretAccessKey
+    secretAccessKey,
+  },
+});
+
+async function uploadFile(fileBuffer, fileName, mimetype) {
+  // console.log(`File Buffer is: ${fileBuffer}`);
+  // console.log(`File name is: ${fileName}`);
+  // console.log(`File type is: ${mimetype}`);
+
+  try {
+    const uploadParams = {
+      Bucket: bucketName,
+      Body: fileBuffer,
+      Key: fileName,
+      ContentType: mimetype,
+    };
+    await s3Client.send(new PutObjectCommand(uploadParams));
+    return `{"message": "File upload succeeded"}`;
+  } catch (err) {
+    return `{"message": "File upload failed"}`;
   }
-})
-
-function uploadFile(fileBuffer, fileName, mimetype) {
-    // console.log(`File Buffer is: ${fileBuffer}`);
-    // console.log(`File name is: ${fileName}`);
-    // console.log(`File type is: ${mimetype}`);
-
-    try{
-        const uploadParams = {
-            Bucket: bucketName,
-            Body: fileBuffer,
-            Key: fileName,
-            ContentType: mimetype
-        }   
-            s3Client.send(new PutObjectCommand(uploadParams));
-            return `{"message": "File upload succeeded"}`
-        } catch(err){
-            return `{"message": "File upload failed"}`
-    }
 }
 
-function deleteFile(fileName) {
-  try{
+await function deleteFile(fileName) {
+  try {
     const deleteParams = {
-        Bucket: bucketName,
-        Key: fileName,
-      }   
-      s3Client.send(new DeleteObjectCommand(deleteParams));
-      return `{"message": "File delete succeeded"}`
-    } catch {
-        return `{"message": "File delete failed"}`
+      Bucket: bucketName,
+      Key: fileName,
+    };
+    s3Client.send(new DeleteObjectCommand(deleteParams));
+    return `{"message": "File delete succeeded"}`;
+  } catch {
+    return `{"message": "File delete failed"}`;
   }
-}
+};
 
 async function getObjectSignedUrl(key) {
   const params = {
     Bucket: bucketName,
-    Key: key
-  }
+    Key: key,
+  };
 
   // https://aws.amazon.com/blogs/developer/generate-presigned-url-modular-aws-sdk-javascript/
   const command = new GetObjectCommand(params);
   //const seconds = 60
-  const url = await getSignedUrl(s3Client, command/*, { expiresIn: seconds }*/);
+  const url = await getSignedUrl(
+    s3Client,
+    command /*, { expiresIn: seconds }*/
+  );
 
-  return url
+  return url;
 }
 
-module.exports = {uploadFile, deleteFile, getObjectSignedUrl};
+module.exports = { uploadFile, deleteFile, getObjectSignedUrl };
