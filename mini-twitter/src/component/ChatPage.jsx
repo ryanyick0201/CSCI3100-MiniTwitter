5f3610c9d9b68e80d59ff39e175df1cdbfc5caed
raymondchoimw <raymondchoimw@gmail.com>
2023-04-13 20:51:27 +0800

    update styling

--- mini-twitter/src/component/ChatPage.jsx
+++ mini-twitter/src/component/ChatPage.jsx
@@ -10,14 +10,14 @@ const socket = io(SOCKET_SERVER_URL);
 
 const useStyles = makeStyles({
   pageContainer: {
-    position: "fixed",
-    top: "5%",
-    left: "5%",
+    position: "relative",
+    top: "5vh",
+    //left: "5%",
     borderStyle: "solid",
     borderColor: "green",
 
     display: "flex",
-    width: "90vw",
+    //width: "90vw",
     height: "90vh",
     backgroundColor: "#263238",
   },
